392f042dcfc8a076def9580989aeb25888061368
Bishal Halder <84507718+vishalh016@users.noreply.github.com>
2021-07-16 20:24:03 +0530

    Update Candidate.java

--- Dashboard/Java Programming Funcamentals and Soft Skills/Applying Object Oriented Concepts in java/Register a Candidate - User defined Exception(with throw and throws)/Candidate.java
+++ Dashboard/Java Programming Funcamentals and Soft Skills/Applying Object Oriented Concepts in java/Register a Candidate - User defined Exception(with throw and throws)/Candidate.java
@@ -1,29 +1,38 @@
+package ict2;
+
 public class Candidate {
-    private String name;
-    private String gender;
-    private double expectedSalary;
+	private String name;
+	private String gender;
+	private double expectedSalary;
+	
+	public Candidate(String name,String gender,double expectedSalary ) {
+		this.name=name;
+		this.gender=gender;
+		this.expectedSalary=expectedSalary;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
-    public double getExpectedSalary() {
-        return expectedSalary;
-    }
+	public double getExpectedSalary() {
+		return expectedSalary;
+	}
 
-    public void setExpectedSalary(double expectedSalary) {
-        this.expectedSalary = expectedSalary;
-    }
+	public void setExpectedSalary(double expectedSalary) {
+		this.expectedSalary = expectedSalary;
+	}
+	
 }
